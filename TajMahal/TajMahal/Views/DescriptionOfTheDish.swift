5f2a5df056c3333fbc5c3c0629df0c3c677530f3
Kevin Massol <kevin.massol@icloud.com>
2024-03-25 16:28:45 +0100

    Réorganisation du fichier ViewModel pour une meilleur lecture

--- TajMahal/TajMahal/Views/DescriptionOfTheDish.swift
+++ TajMahal/TajMahal/Views/DescriptionOfTheDish.swift
@@ -1,5 +1,5 @@
 //
-//  DetailMealView.swift
+//  DescriptionOfTheDish.swift
 //  TajMahal
 //
 //  Created by KEVIN on 25/03/2024.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct DetailMealView: View {
+struct DescriptionOfTheDish: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    DetailMealView()
+    DescriptionOfTheDish()
 }

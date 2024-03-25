b360ea94967693976c1008a485fd234a285bf2de
Kevin Massol <kevin.massol@icloud.com>
2024-03-25 11:33:49 +0100

    Ajout des assets

---
+++ TajMahal/TajMahal/Views/DetailMealView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailMealView.swift
+//  TajMahal
+//
+//  Created by KEVIN on 25/03/2024.
+//
+
+import SwiftUI
+
+struct DetailMealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailMealView()
+}

b78f9a726371c62314a9fa3d5495c5ad24df3f89
Kevin Massol <kevin.massol@icloud.com>
2024-03-25 11:37:39 +0100

    Mise à jour de la page d'accueil "WelcomeView"

--- TajMahal/TajMahal/Views/WelcomeView.swift
+++ TajMahal/TajMahal/Views/WelcomeView.swift
@@ -13,15 +13,25 @@ struct WelcomeView: View {
         NavigationStack {
             PresentationImageView()
             Spacer()
+            InformationView()
+            Spacer()
+            DetailRestaurantView(icon: "horloge", TextOnTheLeft: "Mardi", TextOnTheRight: "11h30 - 14h30・ 18h30 - 22h00")
+            DetailRestaurantView(icon: "service", TextOnTheLeft: "Type de Service", TextOnTheRight: "À emporter")
+            DetailRestaurantView(icon: "localisation", TextOnTheLeft: "12 Avenue de la Brique - 75010 Paris", TextOnTheRight: "")
+            DetailRestaurantView(icon: "globe", TextOnTheLeft: "www.tajmahal.fr", TextOnTheRight: "")
+            DetailRestaurantView(icon: "phone", TextOnTheLeft: "06 12 34 56 78", TextOnTheRight: "")
+            Spacer()
             NavigationLink {
                 MenuView()
             } label : {
                 Text("Accéder au menu")
                     .foregroundStyle(Color.white)
+                    .font(.custom("PlusJakartaSans-Bold-700", size: 16))
             }
             .frame(width: 335, height: 40)
             .background(.customRed)
-            .cornerRadius(10)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            Spacer()
         }
     }
 }

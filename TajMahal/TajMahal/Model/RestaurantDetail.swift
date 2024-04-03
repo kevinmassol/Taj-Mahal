1a1bcdc981dd973daa365c8fcb4ef9326e54868a
Kevin Massol <kevin.massol@icloud.com>
2024-04-03 23:48:56 +0200

    Création d'une struct pour les informations détaillées du restaurant

---
+++ TajMahal/TajMahal/Model/RestaurantDetail.swift
@@ -0,0 +1,22 @@
+//
+//  RestaurantDetail.swift
+//  TajMahal
+//
+//  Created by KEVIN on 03/04/2024.
+//
+
+import Foundation
+
+struct RestaurantDetail {
+    let icon: String
+    let leftText: String
+    let rightText: String
+}
+
+let details: [RestaurantDetail] = [
+    RestaurantDetail(icon: "horloge", leftText: "Mardi", rightText: "11h30 - 14h30・ 18h30 - 22h00"),
+    RestaurantDetail(icon: "service", leftText: "Type de Service", rightText: "À emporter"),
+    RestaurantDetail(icon: "localisation", leftText: "12 Avenue de la Brique - 75010 Paris", rightText: ""),
+    RestaurantDetail(icon: "globe", leftText: "www.tajmahal.fr", rightText: ""),
+    RestaurantDetail(icon: "phone", leftText: "06 12 34 56 78", rightText: "")
+]

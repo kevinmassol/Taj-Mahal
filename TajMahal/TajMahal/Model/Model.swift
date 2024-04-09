40e1b74b738e8cf2318fa99fee80447d3039c033
Kevin Massol <kevin.massol@icloud.com>
2024-04-09 22:14:28 +0200

    Update de enum avec l'ajout de Int pour chaque cas d'énumération

--- TajMahal/TajMahal/Model/Model.swift
+++ TajMahal/TajMahal/Model/Model.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 // Cette enumération répertorie 3 niveaux de piment
-enum SpiceLevel {
-    case light
-    case medium
-    case hot
+enum SpiceLevel: Int, CaseIterable {
+    case light = 1
+    case medium = 2
+    case hot = 3
 }
 
 // Représente l'objet "plat", qui figure sur la carte du menu

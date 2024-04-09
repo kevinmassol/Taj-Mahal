a3874ecfe941f0cb53e2c4ea357365fcf1f71402
Kevin Massol <kevin.massol@icloud.com>
2024-04-09 22:56:09 +0200

    Ajout de la vue "PepperView" pour afficher les niveux de piments

--- TajMahal/TajMahal/Views/PepperView.swift
+++ TajMahal/TajMahal/Views/PepperView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct PepperView: View {
+    
+    let spiceLevel: SpiceLevel
+    
+    // Fonction pour afficher les images de piment en fonction du niveau de piquant
+    func spiceLevelImages(level: SpiceLevel) -> some View {
+        
+        let redPepperImage = Image(.pimentRed)
+            .resizable()
+            .frame(width: 12, height: 12)
+        let grayPepperImage = Image(.piment)
+            .resizable()
+            .frame(width: 12, height: 12)
+        
+        return HStack(spacing: 2) {
+            ForEach(0..<level.rawValue, id: \.self) { spiceLevel in
+                redPepperImage
+            }
+            ForEach(level.rawValue..<SpiceLevel.hot.rawValue, id: \.self) { spiceLevel in
+                grayPepperImage
+            }
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 11)
+                .frame(width: 74, height: 22)
+                .offset(x: 110, y: -210)
+                .foregroundStyle(.white)
+            spiceLevelImages(level: spiceLevel)
+        }
     }
 }
 
 #Preview {
-    PepperView()
+    PepperView(
+        spiceLevel: .light
+    )
 }
